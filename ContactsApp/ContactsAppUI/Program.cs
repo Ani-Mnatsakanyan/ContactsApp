14e9fd8e5f4f7b4500828bd5e605409a2e43525a
Ани Мнацаканян <Mnatsakanyan300800@mail.ru>
2021-03-09 14:47:56 +0700

    Исправлена ошибка сериализации объекта

--- ContactsApp/ContactsAppUI/Program.cs
+++ ContactsApp/ContactsAppUI/Program.cs
@@ -12,28 +12,29 @@ namespace ContactsAppUI
         [STAThread]
         static void Main()
         {
-           /* Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());*/
-           try
-           {
-               //должен вывести ошибку, после исправить добавив еще одну 9
-               PhoneNumber num = new PhoneNumber(number: 79999999999);
-           }
-           catch (Exception e)
-           {
-               Console.WriteLine(value: e);
-               throw;
-           }
-           Project data = new Project();
-           ProjectManager.SaveToFile(project: data, filename: "filename");
-           Contact contact = new Contact(name: "ani", surname: "mnatsakanyan",
-               email: "Mnatsakanyan300800@mail.ru", idVK: "iamani2000",
-               Convert.ToDateTime("30.08.2000"), 79999999999);
-           Console.WriteLine(value: contact.Name + " " + contact.Surname + " " 
-                                    + contact.Number.Number + " " + contact.Email + " "
-                                    + contact.IdVK + " " + contact.BirthDate);
-           Console.Read();
+            /* Application.EnableVisualStyles();
+             Application.SetCompatibleTextRenderingDefault(false);
+             Application.Run(new Form1());*/
+            try
+            {
+                //должен вывести ошибку, после исправить добавив еще одну 9
+                PhoneNumber num = new PhoneNumber(number: 79999999999);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(value: e);
+                throw;
+            }
+            Project data = new Project();
+            Contact contact = new Contact(name: "ani", surname: "mnatsakanyan",
+                email: "Mnatsakanyan300800@mail.ru", idVK: "iamani2000",
+                new DateTime(2000, 8, 30), 79999999999);
+            Console.WriteLine(value: contact.Name + " " + contact.Surname + " "
+                                     + contact.Number.Number + " " + contact.Email + " "
+                                     + contact.IdVK + " " + contact.BirthDate); 
+            data.Contacts.Add(contact); 
+            ProjectManager.SaveToFile(project: data, filename: "filename");
+            Console.Read();
         }
     }
 }
